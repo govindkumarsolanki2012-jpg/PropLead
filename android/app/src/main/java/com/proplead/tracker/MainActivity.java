cd8c336b1a9c509be40db7c3ef61deca23ac53a2
govindkumarsolanki2012-jpg <govindkumarsolanki2012@gmail.com>
2026-09-11 14:39:42 +0530

    feat: add Google social login support
    Implement onActivityResult in MainActivity to handle Google auth intents for the social login plugin and increment the Android versionCode.

--- android/app/src/main/java/com/proplead/tracker/MainActivity.java
+++ android/app/src/main/java/com/proplead/tracker/MainActivity.java
@@ -1,5 +1,31 @@
 package com.proplead.tracker;
 
+import android.content.Intent;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN &&
+            requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+            PluginHandle handle = getBridge() != null ? getBridge().getPlugin("SocialLogin") : null;
+            if (handle != null) {
+                Plugin plugin = handle.getInstance();
+                if (plugin instanceof SocialLoginPlugin) {
+                    ((SocialLoginPlugin) plugin).handleGoogleLoginIntent(requestCode, data);
+                }
+            }
+        }
+    }
+}
